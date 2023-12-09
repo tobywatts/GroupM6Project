36e2a5e5315aaa890ffb114d7bf92bc4cdee6e7d
tobywatts <tobywatts04@gmail.com>
2023-12-09 19:45:02 +0000

    hardcoding went wrong retrying

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const nodeMailer = require('nodemailer');
+const express = require("express");
+const nodeMailer = require("nodemailer");
 
 const app = express();
 const port = 3000;
@@ -9,47 +9,44 @@ const html = `
     <p> I am testing NodeMailer! </p>
 `;
 
-async function sendEmail () {
-    const transporter = nodeMailer.createTransport({
-        host: 'smtp-mail.outlook.com',
-        port: 587,
-        secure:  false,
-        auth: {
-            user: 'dun23rcu.test@outlook.com',
-            pass: '23TestUEA'
-        }
-    });
+async function sendEmail() {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "dun23rcu.test@outlook.com",
+      pass: "23TestUEA",
+    },
+  });
 
-    const info = await transporter.sendMail({
-        from: 'Jules Test <dun23rcu.test@outlook.com>',
-        to: 'waterylemon420@gmail.com',
-        subject: 'Test 123 Testing',
-        html: html
-    })
+  const info = await transporter.sendMail({
+    from: "Jules Test <dun23rcu.test@outlook.com>",
+    to: "waterylemon420@gmail.com",
+    subject: "Test 123 Testing",
+    html: html,
+  });
 
-    console.log("Message sent: " + info.messageId);
+  console.log("Message sent: " + info.messageId);
 }
 
-
 app.use(express.static(__dirname));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/send-email', (req, res) => {
-    // Trigger the email sending logic when a POST request is made to '/send-email'
-    sendEmail().catch(err => console.error(err));
-    res.sendStatus(200); // Respond with a success status to the client
+app.post("/send-email", (req, res) => {
+  // Trigger the email sending logic when a POST request is made to '/send-email'
+  sendEmail().catch((err) => console.error(err));
+  res.sendStatus(200); // Respond with a success status to the client
 });
 
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname }, (err) => {
-        if (err) {
-            console.log(err);
-        }
-    })
-}); 
-
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log (`App listneing on port ${port}!`)
-})
+  console.log(`App listening on port ${port}!`);
+});
