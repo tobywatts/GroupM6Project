8fb19b145855a62d72d89b1bcedb0f3863449061
metalwork12 <137917557+metalwork12@users.noreply.github.com>
2023-12-11 00:05:16 +0000

    Update test.js

--- app/public/test.js
+++ app/public/test.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("data.json")
     .then((response) => response.json())
     .then((data) => {
-
+      const title = document.createElement("h2");
+      title.textContent = data.homePage.Main.header;
 
       
     });
