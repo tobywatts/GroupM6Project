b27a7516ce2a1d1b295886ea827a6718c9005237
metalwork12 <137917557+metalwork12@users.noreply.github.com>
2023-12-11 21:36:06 +0000

    Update goal3.js

--- app/public/goal3.js
+++ app/public/goal3.js
@@ -1,1 +1,27 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        var title = document.createElement("h2");
+        title.textContent = data.goal3.Main.header;
+        console.log(title)
+  
+        var goalimage = document.createElement("img");
+        goalimage.src = data.goal3.Main.goal3Image.src;
+        console.log(data.goal3.Main.goal3Image.src)
+        //goalimage.className = data.goal1.Main.goal1Image.class;
+  
+        var goaltext = document.createElement("p");
+        goaltext.textContent = data.goal3.Main.goal3text.text;
+        goaltext.className = data.goal3.Main.goal3text.class
+  
+  
+  
+  
+        var src = document.getElementById("info");
+        document.getElementById("info").appendChild(title);
+        document.getElementById("info").appendChild(goalimage);
+        document.getElementById("info").appendChild(goaltext);
+      });
+    });
+    
