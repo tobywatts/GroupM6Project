09c4efa341ee55060370a777b46191455b5e320a
metalwork12 <137917557+metalwork12@users.noreply.github.com>
2023-12-11 00:26:18 +0000

    Update test.js

--- app/public/test.js
+++ app/public/test.js
@@ -15,7 +15,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const goalimage = document.createElement("img");
         goalimage.src = data.goal1.Main.goal1Image.src;
-        goalimage.className = data.goal1.Main.goal1Image.
+        goalimage.className = data.goal1.Main.goal1Image.class;
+      }
+
+      if(name = "goal2"){
+        const title = document.createElement("h2");
+        title.textContent = data.goal2.Main.header;
+
+        const goalimage = document.createElement("img");
+        goalimage.src = data.goal2.Main.goal2Image.src;
+        goalimage.className = data.goal2.Main.goal2Image.class;
+      }
+
+      if(name = "goal3"){
+        const title = document.createElement("h2");
+        title.textContent = data.goal3.Main.header;
+
+        const goalimage = document.createElement("img");
+        goalimage.src = data.goal3.Main.goal3Image.src;
+        goalimage.className = data.goal3.Main.goal3Image.class;
       }
     });
 });
